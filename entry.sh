0b0dfb18fa06f799d6be20a7a3778910d515479c
Garen <30864296+GarenMorbid@users.noreply.github.com>
2022-12-30 16:14:47 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,4 +1,4 @@
 #cd /telegram-bot-asf
 #nohup python3 telegram-asf.py tgbot.toml > /dev/null 2>&1 &
 #cd -
-bash /app/ArchiSteamFarm.sh --process-required --service || bash /app/ArchiSteamFarm-Service.sh  --process-required --service
+bash /app/ArchiSteamFarm.sh --cryptkey 125513 --process-required --service || bash /app/ArchiSteamFarm-Service.sh  --cryptkey 125513 --process-required --service
